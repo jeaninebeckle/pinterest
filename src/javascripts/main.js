4c822e9416ca56361d12857c09707684c441fe79
Jeanine Beckle <jeaninebeckle@gmail.com>
2020-06-25 20:58:32 -0500

    enables firebase and populates more of the file structure for use with future data

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -1,6 +1,14 @@
-import '../styles/main.scss';
-import 'bootstrap';
+import firebase from 'firebase/app';
+import apiKeys from './helpers/apiKeys.json';
 
-$('#button').click(() => {
-  console.error('it worked');
-});
+// import auth from './components/auth/auth';
+// import navbar from './components/navbar/navbar';
+
+import 'bootstrap';
+import '../styles/main.scss';
+
+const init = () => {
+  firebase.initializeApp(apiKeys.firebaseConfig);
+};
+
+init();
