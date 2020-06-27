378c4ee7afc8f4f1abf3f00b1d30ba31dd3e32be
Jeanine Beckle <jeaninebeckle@gmail.com>
2020-06-27 09:49:28 -0500

    boards and home divs are shown appropriately based on login status

--- src/javascripts/helpers/data/authData.js
+++ src/javascripts/helpers/data/authData.js
@@ -3,15 +3,21 @@ import 'firebase/auth';
 
 const authDiv = $('#auth');
 const logoutButton = $('#navbar-logout-button');
+const homeDiv = $('#homePage');
+const boardsDiv = $('#boards');
 
 const checkLoginStatus = () => {
   firebase.auth().onAuthStateChanged((user) => {
     if (user) {
       authDiv.addClass('hide');
+      homeDiv.addClass('hide');
       logoutButton.removeClass('hide');
+      boardsDiv.removeClass('hide');
     } else {
       authDiv.removeClass('hide');
       logoutButton.addClass('hide');
+      boardsDiv.addClass('hide');
+      homeDiv.removeClass('hide');
     }
   });
 };
