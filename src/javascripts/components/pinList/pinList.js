f2fe6a9ab7e07e52f54ddeec002ef954e1da5e2c
Jeanine Beckle <jeaninebeckle@gmail.com>
2020-07-08 18:47:31 -0500

    fixed single pin delete to work correctly

--- src/javascripts/components/pinList/pinList.js
+++ src/javascripts/components/pinList/pinList.js
@@ -3,14 +3,33 @@ import pinMaker from '../pinMaker/pinMaker';
 import utils from '../../helpers/utils';
 
 const removePinEvent = (e) => {
-  const pinId = e.target.closest('.card').id;
+  const pinId = e.target.closest('.dlt').id;
   $(e.target.closest('.card')).hide();
   pinData.deletePin(pinId)
     .then((response) => {
       console.warn(response);
+      // reprintBoards(pinId); // eslint-disable-line
     })
     .catch((err) => console.error('could not delete pin', err));
 };
+
+// const reprintBoards = (boardId) => {
+//   pinData.getPinsByBoardId(boardId)
+//     .then((pins) => {
+//       let domString = `
+//     <div class="card ${boardId}">
+//     `;
+
+//       pins.forEach((pin) => {
+//         domString += pinMaker.pinMaker(pin);
+//       });
+
+//       domString += '</div>';
+
+//       utils.printToDom('#pins', domString);
+//     })
+//     .catch((err) => console.error('could not reprint pins', err));
+// };
 
 const changeClass = () => {
   $('#boards').addClass('hide');
@@ -20,10 +39,11 @@ const changeClass = () => {
 const showPins = (e) => {
   changeClass();
   const boardId = e.target.closest('.card').id;
+
   pinData.getPinsByBoardId(boardId)
     .then((pins) => {
       let domString = `
-      <div class="card">
+      <div class="card ${boardId}">
       `;
 
       pins.forEach((pin) => {
